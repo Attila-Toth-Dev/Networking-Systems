6679677b281529c8b9442eb0899c92b739adcd30
Attila <124107774+SparxSV@users.noreply.github.com>
2024-04-30 16:56:53 +1000

    Updated Main Menu Form size

--- C#/Tutorial WinForms/WinFormsProj/WinFormsProj/MenuForm.Designer.cs
+++ C#/Tutorial WinForms/WinFormsProj/WinFormsProj/MenuForm.Designer.cs
@@ -34,7 +34,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(584, 561);
+            ClientSize = new Size(784, 561);
             Name = "MenuForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "MGC Launcher Menu";
