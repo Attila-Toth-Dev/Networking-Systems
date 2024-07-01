ae739d636bab885e36288dc16080c1d27fce0f55
Attila <attilatoth03@outlook.com>
2024-07-01 15:18:07 +1000

    Removed Initial ENET library and reimported

--- ENet-Project/ENet-Project/main.cpp
+++ ENet-Project/ENet-Project/main.cpp
@@ -4,5 +4,12 @@
 
 int main(int argc, const char* argv)
 {
-	if(enet_initialize() != 0)
+	if (enet_initialize() != 0)
+	{
+		fprintf(stderr, "An error occured while initializing ENet.\n");
+		return EXIT_FAILURE;
+	}
+	atexit(enet_deinitialize);
+
+	return 0;
 }
